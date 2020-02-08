d9124596f69a124874a098c79677c336b63cd442
Adrianmckenzie <starkdeals@yahoo.com>
2020-02-08 11:19:12 -0500

    I deleted scripts and intergrated weather api

---
+++ assets/javascript/weather.js
@@ -0,0 +1,34 @@
+// This is our API key
+var APIKey = "0021273c2fefbe29acd96b3940316e7a";
+
+// Here we are building the URL we need to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=Orlando,Florida&units=imperial&rain=rain.1h&appid=" + APIKey;
+
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
+
+    // Log the queryURL
+    console.log(queryURL);
+
+    // Log the resulting object
+    console.log(response);
+
+            // Transfer content to HTML
+            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+            $("#wind").text("Wind Speed: " + response.wind.speed);
+            $("#humidity").text("Humidity: " + response.main.humidity + "%");
+            $("#temperature").text("Temperature (F) " + response.main.temp);
+            $("#Rain").text("Rain Percentage" + response.main.Rain + "%")
+            
+    
+            // Log the data in the console as well
+            console.log("Wind Speed: " + response.wind.speed);
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + response.main.temp);
+  });
