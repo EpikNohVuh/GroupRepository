0923e481b4ddb6bc5f3c3cdd5d3ec502c8a18f02
Sharon Patten <EpikNohVuh@gmail.com>
2020-02-10 20:36:19 -0500

    updated: index.html, style.css,script.js,readme.md; added weather.js, datepicker.js, maps.js, jquery-min.js, .jpg

--- assets/javascript/script.js
+++ assets/javascript/script.js
@@ -3,7 +3,7 @@ $(document).ready(function () {
 
   // Your web app's Firebase configuration
   var firebaseConfig = {
-    apiKey: "AIzaSyCfiz_DbXYiXTrzVb60KKajtYEhL98aZps",
+    apiKey: mapKey.Firebase,
     authDomain: "orlandodogpark.firebaseapp.com",
     databaseURL: "https://orlandodogpark.firebaseio.com",
     projectId: "orlandodogpark",
@@ -20,13 +20,13 @@ $(document).ready(function () {
 
   // Initial Values
 
-  var breed = "";
-  var park = "";
+  var date = "";
   var time = "";
-  var owner = "";
+  var park = "";
+  var breed = "";
   var comment = "";
 
-  console.log('CURRENT TIME: ' + moment(currentTime).format('hh:mm:ss A'));
+    console.log('CURRENT TIME: ' + moment(currentTime).format('hh:mm:ss A'));
 
   // Capture Button Click
   $("#addDate").on("click", function (event) {
@@ -40,14 +40,20 @@ $(document).ready(function () {
     breed = $("#breed").val().trim();
     comment = $("#comment").val().trim();
 
-    console.log(date);
-    console.log(time);
-    console.log(park);
-    console.log(breed);
-    console.log(comment);
+    // console.log(date);
+    console.log(time.split(":"));
+    
+    
+
+
+
+
+
+    // console.log(park);
+    // console.log(breed);
+    // console.log(comment);
 
     var database = firebase.database();
-<<<<<<< HEAD
 
     // Code for handling the push
     database.ref().push({
@@ -70,13 +76,13 @@ $(document).ready(function () {
 
     var date2 = sv.date;
     var time2 = sv.time;
-    var breed2 = sv.breed;
     var park2 = sv.park;
+    var breed2 = sv.breed;
     var comment2 = sv.comment;
 
     // Add each owners's data into the table
-    $("#playDatePlayBook").append("<tr><td>" + date2 + "</td><td>" + time2 + "</td><td>" + breed2 +
-      "</td><td>" + park2 + "</td><td>" + comment2 + "</td></tr>");
+    $("#playDatePlayBook").append("<tr><td>" + date2 + "</td><td>" + time2 + "</td><td>" + park2 +
+      "</td><td>" + breed2 + "</td><td>" + comment2 + "</td></tr>");
 
     // Handle the errors
   }, function (errorObject) {
@@ -85,108 +91,4 @@ $(document).ready(function () {
   });
 
 }); // END - $(document).ready(function () 
-=======
-  
-  var mykey = config.mykey;
 
-function initMap() {
-    var myLatLng = {lat: 28.525368, lng: -81.397870};
-    var map = new google.maps.Map(document.getElementById('map'), {
-      center: myLatLng,
-      zoom: 11
-});
-
-var marker1 = new google.maps.Marker({
-  position: myLatLng,
-  map: map,
-  title: 'Center of Map'
-});  
-
-var marker2 = new google.maps.Marker({
-  position: {lat: 28.585760, lng: -81.327790},
-  map: map,
-  title: 'Lake Baldwin Dog Park'
-});
-
-var marker3 = new google.maps.Marker({
-  position: {lat: 28.373170, lng: -81.344340},
-  map: map,
-  title: 'Meadow Woods Dog Park'
-});
-
-var marker4 = new google.maps.Marker({
-  position: {lat: 28.541390, lng: -81.316870},
-  map: map,
-  title: 'Park of the Americas'
-}); 
-
-var marker5 = new google.maps.Marker({
-  position: {lat: 28.418210, lng: -81.229200},
-  map: map,
-  title: 'Lake District Dog Park'
-});
-
-var marker6 = new google.maps.Marker({
-  position: {lat: 28.571070, lng: -81.339810},
-  map: map,
-  title: 'Blue Jacket Park'
-});
-
-var marker7 = new google.maps.Marker({
-  position: {lat: 28.545520, lng: -81.296910},
-  map: map,
-  title: 'Yucatan Park'
-});
-
-var marker8 = new google.maps.Marker({
-  position: {lat: 28.497940, lng: -81.320310},
-  map: map,
-  title: 'Barber Park'
-});
-
-var marker9 = new google.maps.Marker({
-  position: {lat: 28.572100, lng: -81.366821},
-  map: map,
-  title: 'Loch Haven Park'
-});
-
-var marker10 = new google.maps.Marker({
-  position: {lat: 28.565820, lng: -81.241680},
-  map: map,
-  title: 'Downey Dog Park'
-});
-
-var marker11 = new google.maps.Marker({
-  position: {lat: 28.553300, lng: -81.350000},
-  map: map,
-  title: 'Lake Druid Park'
-});
-
-var marker12 = new google.maps.Marker({
-  position: {lat: 28.544430, lng: -81.565370},
-  map: map,
-  title: 'West Orange Dog Park'
-});
-
-var marker13 = new google.maps.Marker({
-  position: {lat: 28.533630, lng: -81.360030},
-  map: map,
-  title: 'Greenwood Urban Wetlands'
-});
-
-}    
-marker1.setMap(map);
-marker2.setMap(map);
-marker3.setMap(map);
-marker4.setMap(map);
-marker5.setMap(map);
-marker6.setMap(map);
-marker7.setMap(map);
-marker8.setMap(map);
-marker9.setMap(map);
-marker10.setMap(map);
-marker11.setMap(map);
-marker12.setMap(map);
-marker13.setMap(map);
-
->>>>>>> c7226eed916709db524ce381ec1a9b6c568f3ea3
---
+++ assets/javascript/weather.js
@@ -0,0 +1,30 @@
+// This is our API key
+var APIKey = mapKey.Weather;
+// Here we are building the URL we need to query the database
+var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+  "q=Orlando,Florida&units=imperial&rain=rain.1h&appid=" + APIKey;
+// Here we run our AJAX call to the OpenWeatherMap API
+$.ajax({
+  url: queryURL,
+  method: "GET"
+})
+  // We store all of the retrieved data inside of an object called "response"
+  .then(function(response) {
+    // Log the queryURL
+    console.log(queryURL);
+    // Log the resulting object
+    console.log(response);
+            // Transfer content to HTML
+            $(".city").html("<h1>" + response.name + " Weather Details</h1>");
+            $("#wind").text("Wind Speed: " + response.wind.speed);
+            $("#humidity").text("Humidity: " + response.main.humidity + "%");
+            $("#temperature").text("Temperature (F) " + response.main.temp);
+            // If rain exist then display rain or display weather
+            $("#Rain").text("Rain" + response.weather[0].main + "")
+            
+    
+            // Log the data in the console as well
+            console.log("Wind Speed: " + response.wind.speed);
+            console.log("Humidity: " + response.main.humidity);
+            console.log("Temperature (F): " + response.main.temp);
+  });
